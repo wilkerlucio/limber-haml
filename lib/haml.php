ebca37502a81ac18ee19d64b08ce918ea3cb6a7f
Wilker Lucio <wilkerlucio@gmail.com>
2009-04-29 14:41:49 -0300

    initial commit

---
+++ lib/haml.php
@@ -0,0 +1,216 @@
+<?php
+
+/*
+ * Copyright 2009 Limber Framework
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License. 
+ */
+
+class Haml
+{
+	private $element_stack;
+	private $current_indent;
+	private $buffer;
+	
+	private static $AUTOCLOSE = array("br", "hr", "meta", "link");
+	
+	public function parse($data)
+	{
+		$this->element_stack = array();
+		$this->current_indent = 0;
+		$this->buffer = "";
+		
+		$lines = preg_split("(\r\n|\n|\r)", $data);
+		
+		foreach ($lines as $line_number => $line) {
+			$indent = $this->calculate_indent($line);
+			
+			if ($indent > $this->current_indent) {
+				throw new HamlParseException("Invalid identation level at line {$line_number}");
+			}
+			
+			$line = $this->normalize_line($line);
+			
+			if (preg_match("/^-\s*else/", $line)) {
+				$this->current_indent -= 1;
+				$this->write($this->php_code("else:"));
+				$this->current_indent += 1;
+				
+				continue;
+			}
+			
+			while ($indent < $this->current_indent) {
+				$this->element_pop();
+			}
+			
+			if (preg_match("/^([%#.][a-z#.0-9]+)(\/|=)?(?:\s+\{(.*?)})?(?:\s+(.*))?/i", $line, $matches)) {
+				$attributes = array();
+				
+				preg_match_all("/([%#.])([a-z0-9]+)/i", $matches[1], $operations, PREG_SET_ORDER);
+				
+				$classes = array();
+				$tag = null;
+				
+				foreach ($operations as $op) {
+					$cmd = $op[1];
+					$val = $op[2];
+					
+					switch ($cmd) {
+						case '%':
+							$tag = $val;
+							break;
+						case '#':
+							$attributes["id"] = "\"{$val}\"";
+							break;
+						case '.':
+							$classes[] = $val;
+							break;
+					}
+				}
+				
+				if ($tag === null) {
+					$tag = "div";
+				}
+				
+				if (count($classes) > 0) {
+					$attributes["class"] = '"' . implode(" ", $classes) . '"';
+				}
+				
+				$autoclose = @$matches[2] == '/' || in_array($tag, self::$AUTOCLOSE);
+				$attributes_string = isset($matches[3]) ? $matches[3] : null;
+				$content = isset($matches[4]) ? $matches[4] : "";
+				$multiline = !$autoclose && $this->calculate_indent(@$lines[$line_number + 1]) > $this->current_indent;
+				
+				if ($matches[2] == '=') {
+					$content = $this->php_echo($content);
+				}
+				
+				if ($attributes_string) {
+					preg_match_all("/([a-z][a-z-]*)\s*=\s*(('|\")?(?(3).*?\\3|[^\s]+))/i", $attributes_string, $attr_matches, PREG_SET_ORDER);
+					
+					foreach ($attr_matches as $match) {
+						$attributes[$match[1]] = $match[2];
+					}
+				}
+				
+				if ($multiline) {
+					$this->stack_tag($tag, $attributes);
+				} else {
+					$this->append_tag($tag, $attributes, $content, $autoclose);
+				}
+			} elseif (preg_match("/^([=-])(.*)/", $line, $matches)) {
+				$code = trim($matches[2]);
+				$write = $matches[1] == '=';
+				
+				if (preg_match("/^(?<statement>if|foreach|while|for)\s*(?<params>.*)/", $code, $code_match)) {
+					$statement = $code_match["statement"];
+					$params = $code_match["params"];
+					
+					$this->write($this->php_code("{$statement} ({$params}):"));
+					$this->stack_element($this->php_code("end{$statement}"));
+				} elseif(preg_match("/^else/", $code)) {
+					
+				} else {
+					$fn = $write ? "php_echo" : "php_code";
+					
+					$this->write($this->$fn($code));
+				}
+			} else {
+				$this->write($line);
+			}
+		}
+		
+		while (count($this->element_stack) > 0) {
+			$this->element_pop();
+		}
+		
+		return trim($this->buffer);
+	}
+	
+	private function php_code($code)
+	{
+		return "<?php $code ?>";
+	}
+	
+	private function php_echo($code)
+	{
+		return $this->php_code("echo $code");
+	}
+	
+	private function stack_element($element)
+	{
+		$this->current_indent += 1;
+		$this->element_stack[] = $element;
+	}
+	
+	private function stack_tag($tag, $attributes)
+	{
+		$attr = $this->build_attributes($attributes);
+		
+		$this->write("<{$tag}{$attr}>");
+		
+		$this->stack_element("</{$tag}>");
+	}
+	
+	private function append_tag($tag, $attributes, $content, $autoclose = false)
+	{
+		$attr = $this->build_attributes($attributes);
+		
+		if ($autoclose) {
+			$this->write("<{$tag}{$attr} />");
+		} else {
+			$this->write("<{$tag}{$attr}>{$content}</{$tag}>");
+		}
+	}
+	
+	private function build_attributes($attributes)
+	{
+		$attr = "";
+		
+		foreach ($attributes as $key => $value) {
+			$attr .= " {$key}={$value}";
+		}
+		
+		return $attr;
+	}
+	
+	private function element_pop()
+	{
+		$item = array_pop($this->element_stack);
+		$this->current_indent -= 1;
+		$this->write($item);
+	}
+	
+	private function write($data)
+	{
+		$this->buffer .= str_repeat("\t", $this->current_indent);
+		$this->buffer .= $data;
+		$this->buffer .= "\n";
+	}
+	
+	private function calculate_indent($line)
+	{
+		if (preg_match("/^(\t+)/", $line, $matches)) {
+			return strlen($matches[1]);
+		}
+		
+		return 0;
+	}
+	
+	private function normalize_line($line)
+	{
+		return trim($line);
+	}
+}
+
+class HamlParseException extends Exception {}
---
+++ spec/haml_spec.php
@@ -0,0 +1,440 @@
+<?php
+
+/*
+ * Copyright 2009 Limber Framework
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License. 
+ */
+
+require_once dirname(__FILE__) . "/../lib/haml.php";
+
+function spec_parse(&$spec, $template, $expected)
+{
+	$haml = new Haml();
+	$html = $haml->parse($template);
+	
+	$spec($html)->should->be($expected);
+}
+
+describe("Haml", function($spec) {
+	$spec->context("parsing", function($spec) {
+		$spec->it("should parse simple strings", function($spec, $data) {
+			$template = "some simple string";
+			$expected = "some simple string";
+			
+			spec_parse($spec, $template, $expected);
+		});
+		
+		$spec->it("should parse tags in single line", function($spec, $data) {
+			$template = "%div div content";
+			$expected = "<div>div content</div>";
+			
+			spec_parse($spec, $template, $expected);
+		});
+		
+		$spec->it("should parse many tags in single line", function($spec, $data) {
+			$template = <<<EOS
+%div some content
+%span more content
+EOS;
+			
+			$expected = <<<EOS
+<div>some content</div>
+<span>more content</span>
+EOS;
+			
+			spec_parse($spec, $template, $expected);
+		});
+
+		$spec->it("should parse multi-line elements", function($spec, $data) {
+			$template = <<<EOS
+%div
+	this time
+	the content will be internal
+EOS;
+
+			$expected = <<<EOS
+<div>
+	this time
+	the content will be internal
+</div>
+EOS;
+
+			spec_parse($spec, $template, $expected);
+		});
+
+		$spec->it("should parse nested elements", function($spec, $data) {
+			$template = <<<EOS
+%div
+	this time
+	%b im bold
+	nested
+	%span
+		nesting multiline
+		%textarea
+			alot nesting
+and im at end
+EOS;
+
+			$expected = <<<EOS
+<div>
+	this time
+	<b>im bold</b>
+	nested
+	<span>
+		nesting multiline
+		<textarea>
+			alot nesting
+		</textarea>
+	</span>
+</div>
+and im at end
+EOS;
+
+			spec_parse($spec, $template, $expected);
+		});
+		
+		$spec->it("should close tag in same line if no content is passed", function($spec, $data) {
+			$template = <<<EOS
+%div
+	content
+	%script
+	more content
+	%script
+EOS;
+			
+			$expected = <<<EOS
+<div>
+	content
+	<script></script>
+	more content
+	<script></script>
+</div>
+EOS;
+
+			spec_parse($spec, $template, $expected);
+		});
+		
+		$spec->it("should use tag autoclose when passing a slash after tag name", function($spec, $data) {
+			$template = <<<EOS
+%div
+	content
+	%textarea/
+EOS;
+
+			$expected = <<<EOS
+<div>
+	content
+	<textarea />
+</div>
+EOS;
+
+			spec_parse($spec, $template, $expected);
+		});
+
+		$spec->it("should use automatic autoclose on tags: br, hr, link and meta", function($spec, $data) {
+			$template = <<<EOS
+%div
+	%meta
+	%link
+	%br
+	some text
+	%hr
+	more text
+EOS;
+
+			$expected = <<<EOS
+<div>
+	<meta />
+	<link />
+	<br />
+	some text
+	<hr />
+	more text
+</div>
+EOS;
+
+			spec_parse($spec, $template, $expected);
+		});
+		
+		$spec->context("parsing tag shortcuts", function($spec) {
+			$spec->it("should create a div with ID when passing #", function($spec, $data) {
+				$template = "#content";
+				$expected = '<div id="content"></div>';
+				
+				spec_parse($spec, $template, $expected);
+			});
+			
+			$spec->it("should create a div with class when passing .", function($spec, $data) {
+				$template = '.red';
+				$expected = '<div class="red"></div>';
+				
+				spec_parse($spec, $template, $expected);
+			});
+			
+			$spec->it("should allow to use many classes (.)", function($spec, $data) {
+				$template = '.red.blue';
+				$expected = '<div class="red blue"></div>';
+				
+				spec_parse($spec, $template, $expected);
+			});
+			
+			$spec->it("should accept # and . simultaneos", function($spec, $data) {
+				$template = '#content.red.blue';
+				$expected = '<div id="content" class="red blue"></div>';
+				
+				spec_parse($spec, $template, $expected);
+			});
+			
+			$spec->it("should accept # and . simultaneos to help tag construct", function($spec, $data) {
+				$template = '%span#content.red.blue';
+				$expected = '<span id="content" class="red blue"></span>';
+				
+				spec_parse($spec, $template, $expected);
+			});
+		});
+		
+		$spec->context("parsing tag attributes", function($spec) {
+			$spec->it("should accept one attribute into tags", function($spec, $data) {
+				$template = '%div {id="content"}';
+				$expected = '<div id="content"></div>';
+	
+				spec_parse($spec, $template, $expected);
+			});
+	
+			$spec->it("should accept attribute without quotes", function($spec, $data) {
+				$template = '%div {id = content class="many names" rel=external}';
+				$expected = '<div id=content class="many names" rel=external></div>';
+	
+				spec_parse($spec, $template, $expected);
+			});
+	
+			$spec->it("should accept many attributes into tags", function($spec, $data) {
+				$template = "%div {id='con\"tent' class=\"left'column\"}";
+				$expected = '<div id=\'con"tent\' class="left\'column"></div>';
+
+				spec_parse($spec, $template, $expected);
+			});
+	
+			$spec->it("should accept many attributes with contents", function($spec, $data) {
+				$template = <<<EOS
+%div {id='con"tent' class="true"} some content
+%div
+	%span {class="internal"}
+		with nested
+		%b content
+EOS;
+	
+				$expected = <<<EOS
+<div id='con"tent' class="true">some content</div>
+<div>
+	<span class="internal">
+		with nested
+		<b>content</b>
+	</span>
+</div>
+EOS;
+	
+				spec_parse($spec, $template, $expected);
+			});
+		});
+		
+		$spec->context("parsing php code", function($spec) {
+			$spec->it("should print php strings", function($spec, $data) {
+				$template = '= "Hello World!"';
+				$expected = '<?php echo "Hello World!" ?>';
+
+				spec_parse($spec, $template, $expected);
+			});
+			
+			$spec->it("should print php strings into tags", function($spec, $data) {
+				$template = '%div= "Hello World!"';
+				$expected = '<div><?php echo "Hello World!" ?></div>';
+
+				spec_parse($spec, $template, $expected);
+			});
+			
+			$spec->it("should execute php code", function($spec, $data) {
+				$template = '- $name = "Person"';
+				$expected = '<?php $name = "Person" ?>';
+
+				spec_parse($spec, $template, $expected);
+			});
+			
+			$spec->it("should use if statement as block level", function($spec, $data) {
+				$template = <<<EOS
+%div
+	- if 1 != 0
+		yes, 1 is not 0
+EOS;
+				$expected = <<<EOS
+<div>
+	<?php if (1 != 0): ?>
+		yes, 1 is not 0
+	<?php endif ?>
+</div>
+EOS;
+
+				spec_parse($spec, $template, $expected);
+			});
+			
+			$spec->it("should use if/else statement as block level", function($spec, $data) {
+				$template = <<<EOS
+%div
+	- if 1 != 0
+		yes, 1 is not 0
+	- else
+		omg! holly crap!
+EOS;
+				$expected = <<<EOS
+<div>
+	<?php if (1 != 0): ?>
+		yes, 1 is not 0
+	<?php else: ?>
+		omg! holly crap!
+	<?php endif ?>
+</div>
+EOS;
+
+				spec_parse($spec, $template, $expected);
+			});
+			
+			$spec->it("should use for statement as block level", function($spec, $data) {
+				$template = <<<EOS
+%div
+	- for \$i = 0; \$i < 10; \$i++
+		looping
+		= \$i
+EOS;
+				$expected = <<<EOS
+<div>
+	<?php for (\$i = 0; \$i < 10; \$i++): ?>
+		looping
+		<?php echo \$i ?>
+	<?php endfor ?>
+</div>
+EOS;
+
+				spec_parse($spec, $template, $expected);
+			});
+			
+			$spec->it("should use while statement as block level", function($spec, $data) {
+				$template = <<<EOS
+%div
+	- \$i = 0
+	- while \$i < 10
+		looping
+		= \$i
+		- \$i++
+EOS;
+				$expected = <<<EOS
+<div>
+	<?php \$i = 0 ?>
+	<?php while (\$i < 10): ?>
+		looping
+		<?php echo \$i ?>
+		<?php \$i++ ?>
+	<?php endwhile ?>
+</div>
+EOS;
+
+				spec_parse($spec, $template, $expected);
+			});
+			
+			$spec->it("should use foreach statement as block level", function($spec, $data) {
+				$template = <<<EOS
+%div
+	- foreach \$people as \$person
+		= \$person->name
+EOS;
+				$expected = <<<EOS
+<div>
+	<?php foreach (\$people as \$person): ?>
+		<?php echo \$person->name ?>
+	<?php endforeach ?>
+</div>
+EOS;
+
+				spec_parse($spec, $template, $expected);
+			});
+		});
+		
+			
+		$spec->it("should work full integrated features", function($spec, $data) {
+			$template = <<<EOS
+%html
+	%head
+		%title Title of Page
+		%meta {http-equiv="Content-Type" content="text/html; charset=utf-8"}
+	%body
+		#all
+			.bg
+				- if \$user
+					Welcome
+					= \$user
+				#ct
+					page content
+					
+					.requests Your requests:
+					
+					- foreach \$user->requests as \$request
+						.request= \$request
+					
+					.box.logo
+						%h1
+							%a {href="http://some_page" title="Page"} Page
+		#bottom
+			%h3 Thanks for visit
+EOS;
+			
+			$expected = <<<EOS
+<html>
+	<head>
+		<title>Title of Page</title>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	</head>
+	<body>
+		<div id="all">
+			<div class="bg">
+				<?php if (\$user): ?>
+					Welcome
+					<?php echo \$user ?>
+				<?php endif ?>
+				<div id="ct">
+					page content
+					
+					<div class="requests">Your requests:</div>
+					
+					<?php foreach (\$user->requests as \$request): ?>
+						<div class="request"><?php echo \$request ?></div>
+					<?php endforeach ?>
+					
+					<div class="box logo">
+						<h1>
+							<a href="http://some_page" title="Page">Page</a>
+						</h1>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div id="bottom">
+			<h3>Thanks for visit</h3>
+		</div>
+	</body>
+</html>
+EOS;
+
+			spec_parse($spec, $template, $expected);
+		});
+	});
+});
